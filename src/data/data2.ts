f639ae5ffb869a2c8463457b183b42e15313cd70
2k54 <cusing335@gmail.com>
2023-10-11 21:29:14 +0900

    img path fix

--- src/data/data2.ts
+++ src/data/data2.ts
@@ -2,19 +2,19 @@ export const data2 = [
   {
     id: "data2-001",
     title: "かかかかか",
-    img: "/public/img/img4.webp",
+    img: "img/img4.webp",
     link: "",
   },
   {
     id: "data2-002",
     title: "ききききき",
-    img: "/public/img/img5.webp",
+    img: "img/img5.webp",
     link: "",
   },
   {
     id: "data2-003",
     title: "くくくくく",
-    img: "/public/img/img6.webp",
+    img: "img/img6.webp",
     link: "",
   },
 ];
